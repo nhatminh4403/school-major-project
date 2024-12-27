04f73fe834cee054fe2464b45941f8434d448179
Phạm Trần Nhật Minh <145756375+nhatminh4403@users.noreply.github.com>
2024-12-27 20:26:04 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace school_major_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
